5d852762181d74d86774b87087a8f22a1e9a2f89
Scradle <152749477+Scradle@users.noreply.github.com>
2024-04-02 20:25:25 +0200

    ajout de header et footer

---
+++ footer.php
@@ -0,0 +1,15 @@
+</div><!-- #content -->
+
+<footer id="colophon" class="site-footer">
+    <div class="site-info">
+        <?php
+        /* translators: %s: WordPress name and link */
+        printf( esc_html__( 'Proudly powered by %s', 'mon-theme' ), '<a href="' . esc_url( __( 'https://wordpress.org/', 'mon-theme' ) ) . '">WordPress</a>' );
+        ?>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
