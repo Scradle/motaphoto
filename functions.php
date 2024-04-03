935622d2689b5d8a30a862069be80e6b50aeb11e
Scradle <152749477+Scradle@users.noreply.github.com>
2024-04-03 18:27:59 +0200

    ajout de dossiers et import des fonts

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 // Enregistrer les scripts et les styles
-function mon_theme_scripts() {
+function theme_enqueue_scripts() {
     // Enregistrer les scripts
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array(), '1.0', true );
+    wp_enqueue_script( 'script', get_template_directory_uri() . 'assets/js/script.js', array(), '1.0', true );
     
     // Enregistrer les styles
     wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '1.0' );
+    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/assets/css/custom-style.css', array(), '1.0' );
 }
-add_action( 'wp_enqueue_scripts', 'mon_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
