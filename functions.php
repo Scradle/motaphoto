3ac5e3b6e766683e92e76cd6b3a616449d3f7158
Scradle <152749477+Scradle@users.noreply.github.com>
2024-04-04 00:50:01 +0200

    intégration du header

--- functions.php
+++ functions.php
@@ -1,11 +1,24 @@
 <?php
+
 // Enregistrer les scripts et les styles
 function theme_enqueue_scripts() {
     // Enregistrer les scripts
-    wp_enqueue_script( 'script', get_template_directory_uri() . 'assets/js/script.js', array(), '1.0', true );
+    //wp_enqueue_script( 'script', get_template_directory_uri() . 'assets/js/script.js', array(), '1.0', true );
     
     // Enregistrer les styles
     wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '1.0' );
     wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/assets/css/custom-style.css', array(), '1.0' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
+
+//Enregistrer les menus wordpress
+function register_my_menus() {
+    register_nav_menus(
+        array(
+            'menu-principal' => __( 'Menu Principal' ),
+            'menu-secondaire' => __( 'Menu Secondaire' ),
+            // Ajoutez d'autres emplacements de menu si nécessaire
+        )
+    );
+}
+add_action( 'after_setup_theme', 'register_my_menus' );
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<header id="masthead" class="site-header">
+    <div class="site-branding">
+        <img class="logo" src="<?php echo get_template_directory_uri() . './assets/img/logo.png'; ?> " alt="logo">
+        <nav id="site-navigation" class="main-navigation">
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'menu-principal',
+                'menu_id'        => 'header-menu',
+            ) );
+            ?>
+        </nav><!-- #site-navigation -->
+    </div>
+    
+</header><!-- #masthead -->
+
+<div id="content" class="site-content">
